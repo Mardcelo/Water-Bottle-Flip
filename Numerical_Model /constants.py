6ed2e9c3d591cbffcb04e25e0a60c7f5bd459e3a
Mard <101459063+Mardcelo@users.noreply.github.com>
2024-11-05 22:24:06 +0100

    Create constants.py

---
+++ Numerical_Model /constants.py
@@ -0,0 +1,9 @@
+# constants.py
+
+# Declare constants
+BOTTLE_HEIGHT = 0.28  # Height of the bottle, unit: m
+BOTTLE_RADIUS = 0.04  # Radius of the bottle, unit: m
+BOTTLE_MASS = 0.028  # Mass of the bottle, unit: kg
+WATER_MASS_MAX = 1  # Maximal water in the bottle, unit: kg
+G = 9.81  # Gravity constant on Earth, unit: m/s**2
+# WATER_DENSITY = 1000  # Density of water, unit: kg/m**3
